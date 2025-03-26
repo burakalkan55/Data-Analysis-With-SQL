31540884de178f636c6065452f8dbfac6b880091
burakalkan55 <alkanburak131@gmail.com>
2025-03-26 20:11:26 +0300

    second chapter

--- 2_top_paying_skills.sql
+++ 2_top_paying_skills.sql
@@ -1,10 +1,11 @@
+
+WITH
+    top_paying_jobs AS(
 SELECT 
     job_id,
     job_title,
-    job_location,
-    job_schedule_type,
     salary_year_avg,
-    job_posted_date,
+  
     NAME AS company_name
 FROM
     job_postings_fact
@@ -14,5 +15,12 @@ WHERE
     job_title_short = 'Data Analyst' AND job_country = 'United States' AND salary_year_avg IS NOT NULL
 ORDER BY 
     salary_year_avg DESC
-LIMIT 100;
+LIMIT 100 )
+SELECT top_paying_jobs.* , skills FROM 
+    top_paying_jobs 
+INNER JOIN 
+    skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN 
+    skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC;
 
